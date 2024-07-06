a359d188b3d3fc041d5170544429dd02909ce8e3
kundankumar95 <kundanburnwal32434@gmail.com>
2024-07-06 13:33:01 +0530

    message

---
+++ src/component/popular/popular.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import  './popular.css'
+import data_product from '../assets/data'
+import Item from '../items/item';
+
+const popular = (i) => {
+  return (
+    <div className='popular'>
+        <h1>POPULAR IN WOMEN</h1>
+        <hr/>
+        <div className="popular-item">
+            {data_product.map((item)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default popular
+
---
+++ src/pages/loginSignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loginSignup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default loginSignup
