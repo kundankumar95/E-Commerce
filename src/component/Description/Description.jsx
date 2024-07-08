25550f4a427dd0fd151ea1d324a712135bfbbba3
kundankumar95 <kundanburnwal32434@gmail.com>
2024-07-08 10:19:55 +0530

    commit-something

---
+++ src/component/Description/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Description.css'
+
+const Description = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="discriptionbox-navigator">
+        <div className="discriptionbox-nav-box">Description</div>
+        <div className="discriptionbox-nav-box fade">Reviews {122}</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An e-commerce website offers a wide selection of high-quality products, including fashion, electronics, home goods, and more. We prioritize customer satisfaction with easy navigation, secure transactions, and fast shipping. Our platform features detailed product descriptions, customer reviews, and competitive pricing to ensure a seamless shopping experience. With regular updates and exclusive deals, we aim to provide a diverse and affordable range of items to meet all your needs. Shop with confidence and convenience at our online store.</p>
+        <p>An e-commerce also provides digital services, which offers streaming, free shipping, and exclusive deals. The company operates globally, with a focus on customer convenience through features like one-click purchasing, extensive customer reviews, and a robust delivery network. </p>
+      </div>
+    </div>
+  )
+}
+
+export default Description
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../component/Breadcrum/Breadcrum';
+import ProductDisplay from '../component/ProductDisplay/ProductDisplay';
+import Description from '../component/Description/Description';
 
-const product = () => {
-
-  return (
-    <div >
-      
+const Product = () => {
+   const { all_product } = useContext(ShopContext);
+   const { productId } = useParams();
+   const product = all_product.find((e) => e.id === Number(productId));
+   
+   return (
+    <div className='product'>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product}/>
+      <Description/>
     </div>
-  )
+  );
 }
 
-export default product
+export default Product;
+
