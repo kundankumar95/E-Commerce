de3421c91ed6d632412226031d06be1f20bc005a
kundankumar95 <kundanburnwal32434@gmail.com>
2024-07-19 15:01:45 +0530

    c-7

--- src/pages/loginSignup.jsx
+++ src/pages/loginSignup.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import './CSS/loginsignup.css'
+import React, { useState } from 'react';
+import './CSS/loginsignup.css';
 
+const LoginSignup = () => {
+  const [state, setState] = useState("Login");
 
-const loginSignup = () => {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
+          {state === "Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password'/>
         </div>
         <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login</span></p>
+        {state === "Sign Up"?<p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>:<p className="loginsignup-login">
+          Create an account? <span>Click here</span>
+        </p>}
         <div className="loginsignup-agree">
-          <input type="checkbox"  name='' id=''/>
+          <input type="checkbox" name='agree' id='agree'/>
           <p>By continuing, I agree to the terms of use & privacy policy. </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default loginSignup
+export default LoginSignup;
+
+
